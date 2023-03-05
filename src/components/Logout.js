25c17d5c35b43c75596745890a00ab4c2db69930
aberizuki <zalaarthurn@gmail.com>
2023-03-06 03:34:19 +0700

    update : logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,13 +5,29 @@ export default function Logout({ navigation }) {
   return (
     <View>
       <Pressable
+        style={{
+          backgroundColor: "#6A4029",
+          padding: 22,
+          borderRadius: 20,
+          marginTop: 350,
+          marginHorizontal: 60,
+        }}
         onPress={() => {
           AsyncStorage.removeItem("@userData");
           alert("Logged out");
           navigation.navigate("Start");
         }}
       >
-        <Text>Logout</Text>
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "700",
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          Logout
+        </Text>
       </Pressable>
     </View>
   );
